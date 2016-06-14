cba71cf02f0447ecd3dd29dfa1290eb2e4227416
Avi Charlop <avicharlop@gmail.com>
2016-06-14 10:04:04 -0400

    file write stream

--- chapt5/5_7/chat.js
+++ chapt5/5_7/chat.js
@@ -1,20 +1,20 @@
 var fs = require("fs")
 var people = require("./people.json")
-var file = "chat.log"
+var stream = fs.createWriteStream("chat.log")
 
-// people.forEach(function(person) {
-// 	fs.appendFile(file, `${person.name} has entered the room\n`)
-// 	logChat(person)
-// })
+people.forEach(function(person) {
+	stream.write(`${person.name} has entered the room\n`)
+	logChat(person)
+})
 
 
 
-for(var i = 0; i < 45000; i++){
+for(var i = 0; i < 50000; i++){
 	var p = Math.floor(Math.random() * people.length)
 	logChat(people[p])
 }
 
 function logChat(person) {
 	var i = Math.floor(Math.random() * person.sayings.length)
-	fs.appendFile(file, `${person.name}: ${person.sayings[i]}\n`)
+	stream.write(`${person.name}: ${person.sayings[i]}\n`)
 }
