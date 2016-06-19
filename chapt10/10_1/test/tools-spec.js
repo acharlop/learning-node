0c5d8f0c37f97da2414c596f02a497bcfd94fa33
Avi Charlop <avicharlop@gmail.com>
2016-06-19 14:51:15 -0400

    async testing with mocha and chai

--- chapt10/10_1/test/tools-spec.js
+++ chapt10/10_1/test/tools-spec.js
@@ -1,6 +1,7 @@
 var expect = require("chai").expect
 var tools = require("../lib/tools")
 
+describe("Tools", function() {
 
 	describe("printName()", function() {
 		it("should print the last name first", function() {
@@ -9,3 +10,16 @@ var tools = require("../lib/tools")
 		})
 	})
 	
+	describe("loadWiki()", function() {
+
+		this.timeout(5000)
+
+		it("Load Abraham Lincoln's Wiki Page", function(done) {
+			tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html) {
+				expect(html).to.be.ok
+				done()
+			})
+		})
+	})
+
+})
