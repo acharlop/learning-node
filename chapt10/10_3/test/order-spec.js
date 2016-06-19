900e56b37ddb264cf769eb5d72fbb220143de658
Avi Charlop <avicharlop@gmail.com>
2016-06-19 15:18:50 -0400

    console log testing with sinon spies

--- chapt10/10_3/test/order-spec.js
+++ chapt10/10_3/test/order-spec.js
@@ -3,6 +3,8 @@ var rewire = require("rewire")
 
 var order = rewire("../lib/order")
 
+var sinon = require("sinon")
+
 describe("Ordering Items", function() {
 
 	beforeEach(function() {
@@ -12,11 +14,20 @@ describe("Ordering Items", function() {
 			{sku: "CCC", qty: 3}
 		]
 
+		this.console = {
+			log: sinon.spy()
+		}
+
 		order.__set__("inventoryData", this.testData)
+		order.__set__("console", this.console)
 	})
 
 	it("order an item when there are enough in stock", function(done) {
+
+		var _this = this
+
 		order.orderItem("CCC", 3, function() {
+			expect(_this.console.log.callCount).to.equal(2)
 			done()
 		})
 	})
